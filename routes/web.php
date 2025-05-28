5f44db1344564d39486f9bbb3281355e943716f6
Khaled Abdurahman <khaledabdurahman5@gmail.com>
2025-05-29 00:56:34 +0300

    Sutting up some authentication pages

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,6 @@ Route::controller(ThemeController::class)->name('theme.')->group(function () {
     Route::get('/category', 'category')->name('category');
     Route::get('/contact', 'contact')->name('contact');
     Route::get('/single-Blog', 'singleBlog')->name('singleBlog');
-    Route::get('/login', 'login')->name('login');
-    Route::get('/register', 'register')->name('register');
 });
 
 // Route::get('/', function () {
